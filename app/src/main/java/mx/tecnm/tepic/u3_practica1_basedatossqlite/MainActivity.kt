53827b4f20ebff4f86f6daacbc2cb1f8bda05a62
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-11-06 18:41:25 -0700

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/u3_practica1_basedatossqlite/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.u3_practica1_basedatossqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
